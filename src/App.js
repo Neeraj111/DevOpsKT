29d31af9c87e8c24fb4956f01eaab8f49edb53ae
Neeraj Namol (Bosch Global Software Technologies) <v-nnamol@microsoft.com>
2025-09-30 09:06:58 +0530

    Created test branch and changed version 1 to 2 in test branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is versoin 1
+          This is versoin 2
         </p>
         <a
           className="App-link"
